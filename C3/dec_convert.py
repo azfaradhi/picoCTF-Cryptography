d582d5b4d4841bd98d794c1fc4e2f3ab8be97f09
Radhi <141217488+azfaradhi@users.noreply.github.com>
2024-12-28 12:07:55 +0700

    Add files via upload

---
+++ C3/dec_convert.py
@@ -0,0 +1,10 @@
+cipher = "DLSeGAGDgBNJDQJDCFSFnRBIDjgHoDFCFtHDgJpiHtGDmMAQFnRBJKkBAsTMrsPSDDnEFCFtIbEDtDCIbFCFtHTJDKerFldbFObFCFtLBFkBAAAPFnRBJGEkerFlcPgKkImHnIlATJDKbTbFOkdNnsgbnJRMFnRBNAFkBAAAbrcbTKAkOgFpOgFpOpkBAAAAAAAiClFGIPFnRBaKliCgClFGtIBAAAAAAAOgGEkImHnIl"
+lookup1 = "\n \"#()*+/1:=[]abcdefghijklmnopqrstuvwxyz"
+lookup2 = "ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst"
+asli = ""
+prev = 0
+for char in cipher:
+    curidx = lookup2.index(char)
+    asli += lookup1[(curidx + prev + 40) % 40]
+    prev = (curidx + prev + 40) % 40
+print(asli)
